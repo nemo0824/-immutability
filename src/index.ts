0476bb46732bfc1a5d65f5fa7b2099416e5aa657
임재원 <nemo0824@naver.com>
2024-12-22 16:24:41 +0900

    불변성 함수 gneric 사용, any 사용

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,36 @@
-export function immutable(value: any) {
+export function immutable(value: any): any {
   if (value === null || typeof value !== 'object') {
     return value;
   }
-  let copy: any[] | { [key: string]: any } = Array.isArray(value) ? [] : {};
 
-  for (let key in value) {
-    copy[key] = immutable(value[key]);
+  if (Array.isArray(value)) {
+    return value.map((v): any => immutable(v));
   }
 
+  const copy: { [key: string]: any } = {};
+  for (const key in value) {
+    copy[key] = immutable(value[key]);
+  }
   return copy;
 }
 
+export function useGeneric<T>(value: T): T {
+  if (value === null || typeof value !== 'object') {
+    return value;
+  }
+  if (Array.isArray(value)) {
+    return value.map((v) => useGeneric(v)) as T;
+  }
+
+  const copy = {} as { [key: string]: any };
+  for (const key in value) {
+    copy[key] = useGeneric((value as any)[key]);
+  }
+
+  return copy as T;
+}
+
+// 원본 js 로 만든 깊은 복사
 // let deepcopy = function(v){
 //   if(v === null || typeof v !== 'object'){
 //       return v
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,4 @@
-import { immutable } from '../src';
+import { immutable, useGeneric } from '../src';
 
 describe('테스트 그룹 이름', () => {
   it('더하기 테스트', () => {
@@ -31,5 +31,48 @@ describe('테스트 그룹 이름', () => {
   it('object(얕은복사) 를 넣었을때 object(얕은복사)로 나와야한다 테스트', () => {
     const result = immutable({ a: 1, b: 2 });
     expect(result).toEqual({ a: 1, b: 2 });
+  });
+
+  it('object(깊은복사) 를 넣었을때 object(깊은복사)로 나와야한다 테스트', () => {
+    const result = immutable({ a: 1, b: 2, c: { cccc: 33333 } });
+    expect(result).toEqual({ a: 1, b: 2, c: { cccc: 33333 } });
+  });
+
+  it('Array(얕은복사) 를 넣었을때 Array(얕은복사)로 나와야한다 테스트', () => {
+    const result = immutable([1, 2, 3, 4]);
+    expect(result).toEqual([1, 2, 3, 4]);
+  });
+
+  it('Array(깊은복사) 를 넣었을때 Array(깊은복사)로 나와야한다 테스트', () => {
+    const result = immutable([1, 2, 3, 4, [5, 6, [7, 8]]]);
+    expect(result).toEqual([1, 2, 3, 4, [5, 6, [7, 8]]]);
+  });
+
+  it('Array, Object(깊은복사) 를 넣었을때 Array, Object(깊은복사)로 나와야한다 테스트', () => {
+    const result = immutable({
+      a: 1,
+      b: { c: 2 },
+      d: [3, 4, { e: 5 }],
+    });
+    expect(result).toEqual({
+      a: 1,
+      b: { c: 2 },
+      d: [3, 4, { e: 5 }],
+    });
+  });
+
+  it('origin 에서 불변성함수 적용시 result값 변경했을대 같은지', () => {
+    const origin = { a: 10, b: 20, c: { ccc: 1111 } };
+    const result = immutable(origin);
+
+    result.c.ccc = 9999;
+
+    expect(origin).toEqual({ a: 10, b: 20, c: { ccc: 1111 } });
+  });
+  // use Generic
+
+  it('number를 넣었을때 number로 나와야한다 테스트', () => {
+    const result = useGeneric(10);
+    expect(result).toEqual(10);
   });
 });
